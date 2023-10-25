775a1181e8e0cf7bd029f5a31948d8fb02ef0a86
Bøgh <mboghl21@student.aau.dk>
2023-10-25 12:07:50 +0200

    Segmented code as 4 files with TO DO lists

---
+++ NODE_visualizer.py
@@ -0,0 +1,6 @@
+"""
+TO DO:
+ - Get settings from 'NODE_settings.py' on topic 'settings/NODE_visualizer'
+ - Recieve EEG data from topic 'EEG_datastream'
+ - Make UI with visualisation of detected mental command and its power (Eg. 'Lift' or 'Drop' and power 0-100)
+"""
